dd8e8c95e3092012572c0b07e398379e85f666da
seanemattson <seanemattson@yahoo.com>
2023-08-04 11:52:36 -0500

    cyclical error in api-gateway/main.tf

--- infrastructure/modules/api-gateway/main.tf
+++ infrastructure/modules/api-gateway/main.tf
@@ -1,28 +1,28 @@
 locals {
   paths = [
     {
-      name   = "contact"
-      parent = null
+      name    = "contact"
+      parent  = null
       methods = ["OPTIONS", "POST"]
     },
     {
-      name   = "{id}"
-      parent = "contact"
+      name    = "{id}"
+      parent  = "contact"
       methods = ["OPTIONS", "GET", "PUT", "DELETE"]
     },
     {
-      name   = "contacts"
-      parent = null
+      name    = "contacts"
+      parent  = null
       methods = ["OPTIONS", "GET"]
     },
     {
-      name   = "health"
-      parent = null
+      name    = "health"
+      parent  = null
       methods = ["OPTIONS", "GET"]
     },
     {
-      name   = "addImage"
-      parent = null
+      name    = "addImage"
+      parent  = null
       methods = ["OPTIONS", "POST"]
     }
   ]
@@ -47,9 +47,12 @@ resource "aws_api_gateway_resource" "path" {
   for_each = { for path in local.paths : path.name => path }
 
   rest_api_id = aws_api_gateway_rest_api.cay_api_gateway.id
-  parent_id   = each.value.parent != null ? aws_api_gateway_resource.path[each.value.parent].id : aws_api_gateway_rest_api.cay_api_gateway.root_resource_id
+  parent_id   = each.value.name == "{id}" ? aws_api_gateway_resource.path["contact"].id : (each.value.parent != null 
+                      ? aws_api_gateway_resource.path[each.value.parent].id 
+                      : aws_api_gateway_rest_api.cay_api_gateway.root_resource_id)
   path_part   = each.value.name
 }
+
 
 resource "aws_api_gateway_method" "method" {
   for_each = {
@@ -134,4 +137,69 @@ resource "aws_api_gateway_integration_response" "actual" {
   response_parameters = {
     "method.response.header.Access-Control-Allow-Origin" = "'*'"
   }
+}
+
+
+
+
+// these were in the wrong file, making sure they line up correctly with this one
+
+resource "aws_api_gateway_integration" "contact_post" {
+  rest_api_id = aws_api_gateway_rest_api.cay_api_gateway.id
+  resource_id = aws_api_gateway_resource.path["contact"].id
+  http_method = aws_api_gateway_method.method["contact_POST"].http_method
+  
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = var.contactsFunct_lambda_arn
+}
+
+resource "aws_api_gateway_integration" "contact_id_get" {
+  rest_api_id = aws_api_gateway_rest_api.cay_api_gateway.id
+  resource_id = aws_api_gateway_resource.path["{id}"].id
+  http_method = aws_api_gateway_method.method["{id}_GET"].http_method
+  
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
+  uri                     = var.contactsFunct_lambda_arn
+}
+
+resource "aws_api_gateway_integration" "contact_id_put" {
+  rest_api_id = aws_api_gateway_rest_api.cay_api_gateway.id
+  resource_id = aws_api_gateway_resource.path["{id}"].id
+  http_method = aws_api_gateway_method.method["{id}_PUT"].http_method
+  
+  integration_http_method = "PUT"
+  type                    = "AWS_PROXY"
+  uri                     = var.contactsFunct_lambda_arn
+}
+
+resource "aws_api_gateway_integration" "contact_id_delete" {
+  rest_api_id = aws_api_gateway_rest_api.cay_api_gateway.id
+  resource_id = aws_api_gateway_resource.path["{id}"].id
+  http_method = aws_api_gateway_method.method["{id}_DELETE"].http_method
+  
+  integration_http_method = "DELETE"
+  type                    = "AWS_PROXY"
+  uri                     = var.contactsFunct_lambda_arn
+}
+
+resource "aws_api_gateway_integration" "contacts_get" {
+  rest_api_id = aws_api_gateway_rest_api.cay_api_gateway.id
+  resource_id = aws_api_gateway_resource.path["contacts"].id
+  http_method = aws_api_gateway_method.method["contacts_GET"].http_method
+  
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
+  uri                     = var.contactsFunct_lambda_arn
+}
+
+resource "aws_api_gateway_integration" "addImage_post" {
+  rest_api_id = aws_api_gateway_rest_api.cay_api_gateway.id
+  resource_id = aws_api_gateway_resource.path["addImage"].id
+  http_method = aws_api_gateway_method.method["addImage_POST"].http_method
+  
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = var.imageFunct_lambda_arn
 }
